373609614d3ab70676ae5be5c897d576a6358718
Sumit Khandelwal <sumitgniit@gmail.com>
2021-08-03 19:18:18 +0530

    new note added

--- notes-app/src/Components/Notes/NewNote/AddNewNote.js
+++ notes-app/src/Components/Notes/NewNote/AddNewNote.js
@@ -1,17 +1,39 @@
 import React, {useState} from 'react';
 
-const AddNewNote = () => {
+const AddNewNote = (props) => {
     const [title, setTitle] = useState("")
+    const [amount, setAmount] = useState(0)
+    const [createdAt, setCreatedAt] = useState("")
+    // const [state, setState] = useState({
+    //     title : "",
+    //     amount : 0,
+    //     createdAt : ""
+    // })
 
     const titleChangeHandler = (event) => {
         setTitle(event.target.value);
+        // setState({
+        //     ...state,
+        //     title : event.target.value
+        // })
+    }
+    const amountChangeHandler = event =>{
+        setAmount(event.target.value);
+    }
+    const createdAtChangeHandler = event =>{
+        setCreatedAt(event.target.value)
+    }
+    const submitHandler = (event) => {
+        event.preventDefault()
+        props.onNewNote({title, amount, createdAt : new Date(createdAt)})
     }
     return (
         <div className="row">
             <div className="col-sm-8 col-md-6 offset-sm-2 offset-md-3">
                 <div className="card">
                     <div className="card-body">
-                        <form>
+                        <form onSubmit = {submitHandler}>
+                        {/* Title */}
                             <div className="form-group">
                                 <label>Title</label>
                                 <input type="text" 
@@ -19,7 +41,27 @@ const AddNewNote = () => {
                                     value = {title}
                                     onChange = {titleChangeHandler}
                                     className="form-control"/>
-                                    Modified Value - {title}
+                            </div>
+                            {/* Amount */}
+                            <div className="form-group">
+                                <label>Amount</label>
+                                <input type="number" 
+                                    name="amount"
+                                    value = {amount}
+                                    onChange = {amountChangeHandler}
+                                    className="form-control"/>
+                            </div>
+                            {/* Created At */}
+                            <div className="form-group">
+                                <label>Date</label>
+                                <input type="date" 
+                                    name="date"
+                                    value = {createdAt}
+                                    onChange = {createdAtChangeHandler}
+                                    className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <input type="submit" />
                             </div>
                         </form>
                     </div>
--- notes-app/src/Components/Notes/Notes.js
+++ notes-app/src/Components/Notes/Notes.js
@@ -1,31 +1,38 @@
-
+import React, { useState } from 'react';
 import NoteItem from './NoteItem/NoteItem';
 import AddNewNote from './NewNote/AddNewNote';
 
-const notes = () => {
+const INITIAL_NOTES = [
+  {
+    id: 1,
+    title: "shopping",
+    amount : 12.90,
+    createdAt: new Date("Dec 21, 2019"),
+  },
+  {
+    id: 2,
+    title: "grocery",
+    amount : 10.00,
+    createdAt: new Date("Jan 26, 2020"),
+  },
+];
+
+const Notes = () => {
   // console.log("Notes.js")
-  const notes = [
-    {
-      id: 1,
-      title: "shopping",
-      amount : 12.90,
-      createdAt: new Date("Dec 21, 2019"),
-    },
-    {
-      id: 2,
-      title: "grocery",
-      amount : 10.00,
-      createdAt: new Date("Jan 26, 2020"),
-    },
-  ];
+  const [notes, setNotes] = useState(INITIAL_NOTES)
 
   const theParentFunc = (data) => {
     console.log("[THE PARENT FUNCTION]", data)
   }
 
+  const addNewNote = note => {
+    setNotes(prevState => {
+      return [note, ...prevState]
+    })
+  }
   return (
     <div>
-    <AddNewNote />
+    <AddNewNote onNewNote={addNewNote} />
     <div className="row">
       <NoteItem note={notes[0]} 
         onCallParent={theParentFunc} />
@@ -36,4 +43,4 @@ const notes = () => {
   );
 };
 
-export default notes;
+export default Notes;
